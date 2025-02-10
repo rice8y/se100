a12a283ae1d1f5f6c08f5d4fb180b58bdcd2a1b6
rice8y <eitoy1128@mbr.nifty.com>
2025-02-10 21:47:05 +0900

    init

---
+++ docs/ch01/ex11.py
@@ -0,0 +1,25 @@
+import argparse
+import colorama
+from colorama import Fore, Style
+
+colorama.init(autoreset=True)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Search for lines containing a target string in a file.")
+    parser.add_argument("--file", type=str, help="Path to the input file.")
+    parser.add_argument("--tgt", type=str, help="Target string to search for in the file.")
+    return parser.parse_args()
+    
+def grep(file, tgt):
+    with open(file, 'r', encoding='utf-8') as f:
+        for i, line in enumerate(f, start=1):
+            if tgt in line:
+                highlighted = line.replace(tgt, f"{Fore.RED}{tgt}{Style.RESET_ALL}")
+                print(f"Line {i}: {highlighted}", end='')
+
+def main():
+    args = parse_args()
+    grep(args.file, args.tgt)
+    
+if __name__ == "__main__":
+    main()
