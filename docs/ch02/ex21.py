6666962a1093b04c703a47f521aff75ae74b4ecd
rice8y <eitoy1128@mbr.nifty.com>
2025-02-11 22:58:01 +0900

    fix

--- docs/ch02/ex21.py
+++ docs/ch02/ex21.py
@@ -2,7 +2,7 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Check the number of lines in the input file.")
-    parser.add_argument("--file", type=str, required=True, required=True, help="Path to the input file.")
+    parser.add_argument("--file", type=str, required=True, help="Path to the input file.")
     return parser.parse_args()
 
 def count_lines(file):
